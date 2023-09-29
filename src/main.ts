3ebdafc614d41bdea020cbadfa01f3fc839bd7c2
kebi_ko <kebi3477@naver.com>
2023-09-29 20:01:10 +0900

    docs: ssl 인증을 위한 txt 파일 추가 (임시)

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const pkiValidationPath = path.join(__dirname, '../../.well-known/pki-validation');
+    app.use('/.well-known/pki-validation', express.static(pkiValidationPath));
+    console.log(pkiValidationPath)
 
     app.setGlobalPrefix('api');
+    
     app.use('/uploads', express.static(process.env.UPLOAD_DIR));
     app.use(cookieParser());
     app.useGlobalPipes(new ValidationPipe({
